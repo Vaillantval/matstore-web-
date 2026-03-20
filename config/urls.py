4e9c544d8133c23d2b2af5634b92abc03581066a
V. Vaillant <vaillantval1999@gmail.com>
2026-03-20 13:03:53 -0400

    creation de volume railway

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import JsonResponse
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
@@ -33,8 +33,6 @@ urlpatterns = [
     path('health/', health_check),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
